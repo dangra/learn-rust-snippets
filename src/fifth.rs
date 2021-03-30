81eb55735113a098b7b224787b0860f42ad35ad3
Daniel Graña <dangra@gmail.com>
2021-03-30 17:53:36 -0300

    lifetime and pop()

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,8 +1,6 @@
-use std::mem;
-
-pub struct List<T> {
+pub struct List<'a, T> {
     head: Link<T>,
-    tail: Option<&mut Node<T>>,
+    tail: Option<&'a mut Node<T>>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -12,12 +10,12 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<T> List<T> {
+impl<'a, T> List<'a, T> {
     pub fn new() -> Self {
         List { head: None, tail: None }
     }
 
-    pub fn push(&mut self, elem: T) {
+    pub fn push(&'a mut self, elem: T) {
         let new_tail = Box::new(Node {
             elem: elem,
             next: None,
@@ -36,7 +34,42 @@ impl<T> List<T> {
 
         self.tail = new_tail;
     }
+
+    pub fn pop(&'a mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            self.head = head.next;
+            if self.head.is_none() {
+                self.tail = None;
+            }
+            head.elem
+        })
+    }
+
 }
 
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+    }
+}
 
 
